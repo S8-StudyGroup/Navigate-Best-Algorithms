cba14d69065904fc621104164f90b229abb753b9
HeeJeYang <ghjadmb21@gmail.com>
2023-02-25 19:56:16 +0900

    15656번 문제 해결

--- NBA_heeje/2023_02/week_4th/15656.py
+++ NBA_heeje/2023_02/week_4th/15656.py
@@ -1,2 +1,17 @@
 # [BOJ] 15656. N과 M (7)
-# 소요 시간 : 00분
+# 소요 시간 : 20분
+
+
+
+def dfs(ans_list):
+    if len(ans_list) == M:
+        print(*ans_list)
+        return
+    
+    for number in numbers:
+        dfs(ans_list + [number])
+
+N, M = map(int, input().split())
+numbers = sorted(map(int, input().split()))
+
+dfs([])
