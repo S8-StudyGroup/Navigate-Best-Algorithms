ae8663b22aad41a1d06d621ef1daeb0caa16b6d7
HeeJeYang <ghjadmb21@gmail.com>
2023-02-28 05:22:07 +0900

    43163번 문제 해결

--- NBA_taehak/2023_02/week_3rd/43163.py
+++ NBA_taehak/2023_02/week_3rd/43163.py
@@ -1,2 +1,35 @@
 # [Programmers] 43163. 단어 변환
 # 소요 시간 : 30분
+
+from collections import deque
+
+def changable(a_word, b_word):
+    cnt = 0
+    for idx in range(len(a_word)):
+        if a_word[idx] == b_word[idx]:
+            continue
+        else:
+            cnt += 1
+            if cnt > 1:
+                return False
+    return True
+
+def solution(begin, target, words):
+    answer = 0
+
+    if target not in words:
+        return 0
+
+    que = deque([(begin, 0)])
+
+    while que:
+        word, step = que.popleft()
+        if word == target:
+            answer = step
+            break
+
+        for next_word in words:
+            if changable(word, next_word):
+                que.append((next_word, step + 1))
+
+    return answer
