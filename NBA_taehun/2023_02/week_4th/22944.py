c682f41e96a3a76fe0ddb5e9c2b71eb24f39e708
oth54477 <oth5447@naver.com>
2023-02-28 23:29:01 +0900

    22944번 문제 해결

--- NBA_taehun/2023_02/week_4th/22944.py
+++ NBA_taehun/2023_02/week_4th/22944.py
@@ -1,48 +1,104 @@
 # [BOJ] 22944. 죽음의 비
 # 소요 시간 : 00분
 
+# import sys
+# from collections import deque
+
+# input = sys.stdin.readline
+
+# def bfs(start):
+#     dq = deque([(start, 0, H, 0)])
+#     visited = [[0] * N for _ in range(N)]
+#     visited[start[0]][start[1]] = H
+#     while dq:
+#         now, cnt, h, ud = dq.popleft()
+#         r, c = now
+#         for dr, dc in d:
+#             nr, nc = r + dr, c + dc
+#             if 0 <= nr < N and 0 <= nc < N:
+#                 nv = map[nr][nc]
+#                 temp_h, temp_ud = h, ud
+#                 if nv == ".":
+#                     if temp_h + temp_ud < visited[nr][nc]:
+#                         continue
+#                     map[nr][nc] = temp_h + temp_ud
+#                     if temp_ud:
+#                         temp_ud -= 1
+#                     else:
+#                         temp_h -= 1
+#                 elif nv == "U":
+#                     temp_ud = D - 1
+#                 elif nv == "E":
+#                     return cnt + 1
+#                 if temp_h > 0:
+#                     dq.append(((nr, nc), cnt + 1, temp_h, temp_ud))
+
+# d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+# N, H, D = map(int, input().strip().split())
+
+# map = []
+
+# start = (0,0)
+# for r in range(N):
+#     new_list = list(input())
+#     map.append(new_list)
+#     for c in range(N):
+#         if new_list[c] == "S":
+#             start = (r, c)
+
+# count = bfs(start)
+# if not count:
+#     count = -1
+# print(count)
+
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
 def bfs(start):
-    dq = deque([(*start,  0, H, 0)])
+    dq = deque([(start,  0, H, 0)])
+    maps[start[0]][start[1]] = H
     while dq:
-        r, c, cnt, h, ud = dq.popleft()
+        start, cnt, h, ud = dq.popleft()
+        r, c = start
         for dr, dc in d:
             nr, nc = r + dr, c + dc
-            if 0 <= nr < N and 0 <= nc < N:
-                nv = map[nr][nc]
+            if 0 <= nr < N and 0 <= nc < N and (nr ,nc):
+                nv = maps[nr][nc]
+
+                if nv == "E":
+                    print(cnt + 1)
+                    return
+
                 temp_h, temp_ud = h, ud
-                if type(nv) == int:
-                    if temp_h + temp_ud <= nv:
-                        continue
-                    map[nr][nc] = temp_h + temp_ud
-                    if temp_ud:
-                        temp_ud -= 1
-                    else:
-                        temp_h -= 1
-                elif nv == "U":
-                    temp_ud = D - 1
-                elif nv == "E":
-                    return cnt + 1
-                if temp_h > 0:
+                
+                if nv == "U":
+                    temp_ud = D
+                    maps[nr][nc] = temp_h
+
+                if temp_ud:
+                    temp_ud -= 1
+                else:
+                    temp_h -= 1
+
+                if temp_h == 0:
+                    continue
+                
+                if nv == "U" or nv < temp_h:
+                    maps[nr][nc] = temp_h
                     dq.append(((nr, nc), cnt + 1, temp_h, temp_ud))
+    print(-1)
 
 d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 N, H, D = map(int, input().strip().split())
 
-map = [list(map(lambda x: 0 if x == "." else str(x), input().strip())) for _ in range(N)]
+maps = [list(map(lambda x: 0 if x == "." else str(x), input().strip())) for _ in range(N)]
 
 start = (0,0)
 for r in range(N):
     for c in range(N):
-        if map[r][c] == "S":
+        if maps[r][c] == "S":
             start = (r, c)
 
-count = bfs(start)
-if not count:
-    count = -1
-print(count)
-
+bfs(start)
