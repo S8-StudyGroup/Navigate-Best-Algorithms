3f99ec92bb3333472f834891f4a3c155f642044f
oth54477 <oth5447@naver.com>
2023-02-28 19:55:00 +0900

    15656 문제 풀이

--- NBA_taehun/2023_02/week_4th/22944.py
+++ NBA_taehun/2023_02/week_4th/22944.py
@@ -1,2 +1,23 @@
 # [BOJ] 22944. 죽음의 비
 # 소요 시간 : 00분
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def bfs():
+    dq = deque([])
+
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+N, H, D = map(int, input().strip().split())
+
+map = [input() for _ in range(N)]
+
+start = (0,0)
+for r in range(N):
+    for c in range(N):
+        if map[r][c] == "S":
+            start = (r, c)
+
+bfs()
