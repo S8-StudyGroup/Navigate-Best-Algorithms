da147cad3e6fd27dc795a4d141383030688eec5e
HeeJeYang <ghjadmb21@gmail.com>
2023-03-02 07:06:17 +0900

    10870번 문제 해결

--- NBA_heeje/2023_03/week_1st/10870.py
+++ NBA_heeje/2023_03/week_1st/10870.py
@@ -1,2 +1,16 @@
 # [BOJ] 10870. 피보나치 수 5
-# 소요 시간 : 00분
+# 소요 시간 : 10분
+
+
+def fib(n):
+    if n < 2:
+        return n
+    
+    memo = [0, 1]
+    
+    while len(memo) <= n:
+        memo.append(memo[-2] + memo[-1])
+    
+    return memo[-1]
+
+print(fib(int(input())))
