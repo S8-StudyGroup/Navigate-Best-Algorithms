08da5045718f9c4b78074caf626ac5465e788c87
김도언 <vov06144@naver.com>
2023-03-01 23:59:10 +0900

    Feat: 10870번 문제 해결

--- NBA_doeon/2023_03/week_1st/10870.py
+++ NBA_doeon/2023_03/week_1st/10870.py
@@ -1,2 +1,12 @@
 # [BOJ] 10870. 피보나치 수 5
-# 소요 시간 : 00분
+# 소요 시간 : 10분
+
+n = int(input())
+
+dp = [0, 1]
+
+for i in range(2, n + 1):
+    new = dp[i - 2] + dp[i - 1]
+    dp.append(new)
+
+print(dp[n])
