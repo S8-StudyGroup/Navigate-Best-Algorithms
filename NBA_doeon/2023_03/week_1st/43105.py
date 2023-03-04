13012e4f0a9e98ad4144e3693eef6dbdcad2d821
김도언 <vov06144@naver.com>
2023-03-04 23:55:51 +0900

    43106번 문제 해결

--- NBA_doeon/2023_03/week_1st/43105.py
+++ NBA_doeon/2023_03/week_1st/43105.py
@@ -1,2 +1,33 @@
 # [Programmers] 43105. 정수 삼각형
-# 소요 시간 : 00분
+# 소요 시간 : 40분
+
+def solution(triangle):
+    answer = 0
+    sum_case = [[triangle[0]]]
+    
+    for i in range(1, len(triangle)):
+        row = []
+        for j in range(len(triangle[i])):
+            upper = sum_case[i - 1]
+            here_case = []
+            if j - 1 >= 0:
+                a = max(upper[j - 1])
+                here_case.append(a + triangle[i][j])
+            if j < len(upper):
+                b = max(upper[j])
+                here_case.append(b + triangle[i][j])
+            
+            row.append(here_case)
+        sum_case.append(row)
+    
+    floor = sum_case[-1]
+    max_value = 0
+    
+    for idx in range(len(floor)):
+        for length in range(len(floor[idx])):
+            if floor[idx][length] > max_value:
+                max_value = floor[idx][length]
+                
+    answer = max_value
+    
+    return answer
