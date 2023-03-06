ad5222030ad6c4fe04dcfbcbff181d4748bf4011
oth54477 <oth5447@naver.com>
2023-03-06 17:44:16 +0900

    1843번 문제 해결

--- NBA_taehun/2023_03/week_1st/1843.py
+++ NBA_taehun/2023_03/week_1st/1843.py
@@ -1,2 +1,24 @@
 # [Programmers] 1843. 사칙연산
 # 소요 시간 : 00분
+
+from collections import deque
+
+def solution(arr):
+    table = {"+": (lambda x, y: x + y), "-": (lambda x, y: x - y)}
+    dq = deque(arr)
+    result = 0
+    while dq:
+        number = dq.popleft()
+        operator = dq.popleft()
+        
+
+
+    
+    return
+
+def find_minus(idx, arr):
+    for i in range(idx+1:)
+
+
+print(solution(["1", "-", "3", "+", "5", "-", "8"]))
+print(solution(["5", "-", "3", "+", "1", "+", "2", "-", "4"]))
