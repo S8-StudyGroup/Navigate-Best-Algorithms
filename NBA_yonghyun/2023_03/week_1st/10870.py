c9f211f5f5251155fb83c5c5af92407098aa22bc
Yonghyunc <chyh9218@gmail.com>
2023-03-09 21:33:22 +0900

    10870번 문제 해결

--- NBA_yonghyun/2023_03/week_1st/10870.py
+++ NBA_yonghyun/2023_03/week_1st/10870.py
@@ -1,2 +1,12 @@
 # [BOJ] 10870. 피보나치 수 5
 # 소요 시간 : 00분
+
+def fibonacci(n):
+   if n == 0:
+       return 0
+   if n == 1:
+       return 1
+   else:
+       return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(int(input())))
