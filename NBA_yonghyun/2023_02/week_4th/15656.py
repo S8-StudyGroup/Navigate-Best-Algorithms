7454429c0ba457cd79430143af181f2aec1b8dd5
Yonghyunc <chyh9218@gmail.com>
2023-03-09 21:32:45 +0900

    15656번 문제 해결

--- NBA_yonghyun/2023_02/week_4th/15656.py
+++ NBA_yonghyun/2023_02/week_4th/15656.py
@@ -1,2 +1,20 @@
 # [BOJ] 15656. N과 M (7)
 # 소요 시간 : 00분
+
+from collections import deque
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+def sequence(arr=[]):
+    if len(arr) == m:
+        print(*arr)
+        return
+    for num in nums:
+        arr.append(num)
+        sequence(arr)
+        arr.pop()
+
+
+sequence()
