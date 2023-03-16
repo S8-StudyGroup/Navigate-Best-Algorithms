57d95e030f601f0cde6875b5307d039f4f5f5223
happyd918 <0happydeath0@naver.com>
2023-03-17 00:09:42 +0900

    2579번 문제 해결

--- NBA_taehak/2023_03/week_3rd/2579.py
+++ NBA_taehak/2023_03/week_3rd/2579.py
@@ -4,13 +4,16 @@
 # 입력값 : 300개 이하 계단 개수, 10000이하 점수
 stair_cnt = int(input())
 scores = [int(input()) for _ in range(stair_cnt)]
+scores = scores + [0]
 
-# dp : [한칸한칸올라서 온 경우, 두칸한칸올라서 온경우, (1, 2)칸두칸올라서 온 경우],...
-# dp : [두칸 올라야함, 한칸 올라서 한칸한칸으로/ 두칸올라서 두칸으로, 한칸 올라서 두칸한칸으로/ 두칸 올라서 두칸으로]
-dp = [[0, 0, 0] for _ in range(stair_cnt + 2)]
+# dp : [한칸 올라서 온 경우, 두칸 올라서 온 경우],...
+dp = [[0, 0] for _ in range(stair_cnt + 1)]
+dp[0] = [scores[0], scores[0]]
+dp[1][1] = scores[1]
 
-idx = 0
-for idx in range(stair_cnt):
-    case1, case2, case3 = dp[idx]
+for idx in range(stair_cnt - 1):
+    case1, case2 = dp[idx]
+    dp[idx + 1][0] = case2 + scores[idx + 1]
+    dp[idx + 2][1] = max(dp[idx + 2][1], case1 + scores[idx + 2], case2 + scores[idx + 2])
 
-    dp[idx + 1][0] = max(dp[idx + 1][0], case2 + scores[idx + 1])
+print(max(dp[stair_cnt - 1]))
