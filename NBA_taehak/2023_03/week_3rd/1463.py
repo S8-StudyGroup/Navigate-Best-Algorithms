7be26d69b5b2dc384c567d288be1da4b25d5d874
happyd918 <0happydeath0@naver.com>
2023-03-16 12:07:28 +0900

    1463번 문제 해결

--- NBA_taehak/2023_03/week_3rd/1463.py
+++ NBA_taehak/2023_03/week_3rd/1463.py
@@ -1,2 +1,16 @@
 # [BOJ] 1463. 1로 만들기
 # 소요 시간 : 00분
+X = int(input())
+DP = {num : 1e9 for num in range(1, X + 1)}
+DP[1] = 0
+
+num = 1
+for num in range(1, X):
+    cnt_now = DP[num]
+    if num * 3 <= X:
+        DP[num * 3] = min(cnt_now + 1, DP[num * 3])
+    if num * 2 <= X:
+        DP[num * 2] = min(cnt_now + 1, DP[num * 2])
+    DP[num + 1] = min(cnt_now + 1, DP[num + 1])
+
+print(DP[X])
