7329de8535c052ba9a90a38711f06ebbd7e4478e
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-03-22 00:35:56 +0900

    2579번 문제 해결

--- NBA_yonghyun/2023_03/week_3rd/2579.py
+++ NBA_yonghyun/2023_03/week_3rd/2579.py
@@ -1,2 +1,18 @@
 # [BOJ] 2579. 계단 오르기
 # 소요 시간 : 00분
+
+n = int(input())
+stairs = []
+for _ in range(n):
+    stairs.append(int(input()))
+
+dp = [0] * n
+
+if n == 1:
+    print(stairs[0])
+else:
+    dp[0], dp[1] = stairs[0], stairs[0] + stairs[1]
+    for i in range(2, n):
+        dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i-1] + stairs[i])
+
+    print(dp[-1])
