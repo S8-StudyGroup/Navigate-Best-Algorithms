9fa9a6cd63d6b893f6ccde06102bacd63e198414
happyd918 <0happydeath0@naver.com>
2023-03-21 20:53:57 +0900

    1918번 문제 해결

--- NBA_taehak/2023_03/week_3rd/1918.py
+++ NBA_taehak/2023_03/week_3rd/1918.py
@@ -1,2 +1,33 @@
 # [BOJ] 1918. 후위 표기식
 # 소요 시간 : 00분
+string = list(input())
+pm = ['+', '-']
+md = ['*', '/']
+stack=[]
+result=''
+
+for char in string:
+    if char.isalpha():
+        result += char
+    elif char == '(':
+        stack.append(char)
+
+    elif char in md:
+        while stack and (stack[-1] in md):
+            result += stack.pop()
+        stack.append(char)
+
+    elif char in pm:
+        while stack and stack[-1] != '(':
+            result += stack.pop()
+        stack.append(char)
+        
+    elif char == ')':
+        while stack and stack[-1] != '(':
+            result += stack.pop()
+        stack.pop()
+while stack:
+    result += stack.pop()
+
+print(result)
+
