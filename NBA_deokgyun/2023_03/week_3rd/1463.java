2322c8503cb798a7711143312b9eac3899da82c8
dokkyunYU <ydgar001@naver.com>
2023-03-21 21:56:54 +0900

    1463번 문제 해결

---
+++ NBA_deokgyun/2023_03/week_3rd/1463.java
@@ -0,0 +1,25 @@
+// [BOJ] 1463. 1로 만들기
+// 소요 시간 : 00분
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N + 1];
+
+        dp[1] = 0;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
