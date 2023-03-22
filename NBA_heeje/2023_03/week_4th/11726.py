e378fd43de64626771f337a1e8845ae417bfb231
HeeJeYang <ghjadmb21@gmail.com>
2023-03-23 00:45:24 +0900

    11726번 문제 해결

--- NBA_heeje/2023_03/week_4th/11726.py
+++ NBA_heeje/2023_03/week_4th/11726.py
@@ -1,2 +1,13 @@
 # [BOJ] 11726. 2xn 타일링
-# 소요 시간 : 00분
+# 소요 시간 : 10분
+
+n = int(input())
+dp = [0] * (n + 1)
+
+dp[0] = 1
+dp[1] = 1
+
+for d in range(2, n + 1):
+    dp[d] = (dp[d - 2] + dp[d - 1]) % 10007
+
+print(dp[n])
