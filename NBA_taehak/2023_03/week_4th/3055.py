288ca6d88326a673d63ede26254c6eb36336ce7e
happyd918 <0happydeath0@naver.com>
2023-03-24 17:57:29 +0900

    3055번 문제 풀이중

--- NBA_taehak/2023_03/week_4th/3055.py
+++ NBA_taehak/2023_03/week_4th/3055.py
@@ -1,2 +1,32 @@
 # [BOJ] 3055. 탈출
 # 소요 시간 : 00분
+rsize, csize = map(int, input().split())
+area = [input() for _ in range(rsize)]
+
+result = 'KAKTUS'
+
+for r in range(rsize):
+    for c in range(csize):
+        if area[r][c] == 'D':
+            beaver = (r, c)
+        elif area[r][c] == 'S':
+            target = (r, c)
+        elif area[r][c] == '*':
+            water_source = (r, c)
+
+
+def inrange(r, c):
+    '''
+    r: 행
+    c: 열
+    output: 범위안에 있으면 True 아니면 False
+    '''
+    if 0 <= r < rsize and 0 <= c < csize:
+        return True
+    else:
+        return False
+
+
+def bfs(beaver, target, water_source):
+    beaver_visited = [[False] * csize for _ in range(rsize)]    
+    water_visited = [[False] * csize for _ in range(rsize)]
