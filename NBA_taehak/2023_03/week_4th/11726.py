4cc9f8b838370ada62c5015e7f6dae6dce052db5
happyd918 <0happydeath0@naver.com>
2023-03-24 16:59:38 +0900

    11726번 문제 해결

--- NBA_taehak/2023_03/week_4th/11726.py
+++ NBA_taehak/2023_03/week_4th/11726.py
@@ -1,2 +1,8 @@
 # [BOJ] 11726. 2xn 타일링
 # 소요 시간 : 00분
+size = int(input())
+result = [0, 1, 2]
+
+for idx in range(3, size + 1):
+    result.append(result[-1] + result[-2])
+print(result[size]%10007)
