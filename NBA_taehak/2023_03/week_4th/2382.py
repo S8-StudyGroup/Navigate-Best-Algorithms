cca247ee91bfb4b08f61d059f5857fc4ab34f6ae
happyd918 <0happydeath0@naver.com>
2023-03-28 16:02:10 +0900

    2382번 문제 풀이중

--- NBA_taehak/2023_03/week_4th/2382.py
+++ NBA_taehak/2023_03/week_4th/2382.py
@@ -1,2 +1,69 @@
 # [SWEA] 2382. 미생물 격리
 # 소요 시간 : 00분
+from collections import defaultdict
+
+# 상하좌우
+# delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+
+def move(biomes, cell_size):
+    '''
+    Args:
+        biomes: 미생물 군집 정보 딕셔너리
+        cell_size: 한변에 들어가는 셀 개수
+    Returns:
+        한 사이클 이후 새로은 biomes 
+    '''
+    next_biomes = dict()
+
+    for key, item in biomes.items():
+        r, c = key
+        micro_max, di, micro = item
+
+        nr = r + dr[di]
+        nc = c + dc[di]
+
+        # 가장자리
+        if nr in [0, cell_size] or nc in [0, cell_size]:
+            next_biomes[(nr, nc)] = (micro // 2, (di + 2) % 4, micro // 2)
+        # 합쳐질 경우
+        elif (nr, nc) in next_biomes:
+            micro_max_0, di_0, micro_0 = next_biomes[(nr, nc)]
+            if micro > micro_max_0:
+                next_biomes[(nr, nc)] = (micro, di, micro_0 + micro)
+            else:
+                next_biomes[(nr, nc)] = (micro_max_0, di_0, micro_0 + micro)
+        # 그 외
+        else:
+            next_biomes[(nr, nc)] = (micro, di, micro)
+
+    return next_biomes
+        
+
+for case_num in range(1, int(input()) + 1):
+    # NMK 
+    cell_size, time, biome_cnt = map(int, input().split())
+
+    # { (r, c) : (미생물 수(최대값), 방향, 총 합) }
+    biomes = defaultdict()
+    for _ in range(biome_cnt):
+        r, c, micro, di = map(int, input().split())
+        biomes[(r, c)] = (micro, di - 1, micro)
+
+    if case_num != 1:
+        continue
+
+    print(biomes)
+    for _ in range(time):
+        biomes = move(biomes, cell_size)
+        print('=========================================================================')
+        for key, item in biomes.items():
+            print(key, item)
+
+    result = 0
+    for key, item in biomes.items():
+        result += item[2]
+
+    print(f'#{case_num} {result}')
