fa03c38bd4c9a29e0b2e4e261de786cfdc01e12c
HeeJeYang <ghjadmb21@gmail.com>
2023-03-29 20:14:38 +0900

    11727번 문제 해결

--- NBA_deokgyun/2023_04/week_1st/11727.py
+++ NBA_deokgyun/2023_04/week_1st/11727.py
@@ -1,2 +1,13 @@
 # [BOJ] 11727. 2xn 타일링 2
-# 소요 시간 : 00분
+# 소요 시간 : 20분
+
+n = int(input())
+memo = [1]
+
+for i in range(1, n):
+    if i % 2:
+        memo.append((memo[-1] * 2 + 1) % 10007)
+    else:
+        memo.append((memo[-1] * 2 - 1) % 10007)
+
+print(memo[-1])
