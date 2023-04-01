cf597357160bf308b90dc5f5c70118b3e2a365e7
HeeJeYang <ghjadmb21@gmail.com>
2023-04-02 07:11:14 +0900

    2096번 문제 해결

--- NBA_heeje/2023_04/week_1st/2096.py
+++ NBA_heeje/2023_04/week_1st/2096.py
@@ -1,2 +1,21 @@
 # [BOJ] 2096. 내려가기
 # 소요 시간 : 00분
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+# pre_min_dp = [0] * 3
+# pre_max_dp = [0] * 3
+
+min_dp = list(map(int, input().split()))
+max_dp = min_dp[:]
+
+for _ in range(1, N):
+    # pre_min_dp, pre_max_dp = min_dp, max_dp
+    a, b, c = map(int, input().split())
+    min_dp = [a + min(min_dp[0:2]), b + min(min_dp), c + min(min_dp[1:])]
+    max_dp = [a + max(max_dp[0:2]), b + max(max_dp), c + max(max_dp[1:])]
+
+print(max(max_dp), min(min_dp))
