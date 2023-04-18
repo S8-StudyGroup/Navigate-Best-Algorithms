0663b10eac6ff8db1242e419ed66f760c5b9be43
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-04-18 20:38:26 +0900

    kakao_2번 문제 해결

--- NBA_seongbin/2023_04/week_3rd/kakao_2.py
+++ NBA_seongbin/2023_04/week_3rd/kakao_2.py
@@ -1,2 +1,22 @@
 # [Programmers 2023 KAKAO BLIND TEST] 2. 택배 배달과 수거하기
 # 소요 시간 : 00분
+
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+
+    delivery = 0
+    pickup = 0
+
+    for i in range(n-1, -1, -1):
+
+        if deliveries[i] != 0 or pickups[i] != 0:
+
+            while delivery < deliveries[i] or pickup < pickups[i]:
+                delivery += cap
+                pickup += cap
+                answer += (i + 1) * 2
+
+            delivery -= deliveries[i]
+            pickup -= pickups[i]
+
+    return answer
