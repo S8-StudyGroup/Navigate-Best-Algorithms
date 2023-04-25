915d32ca0317649aa09417346f03604a70b2d3cb
HeeJeYang <ghjadmb21@gmail.com>
2023-04-25 20:51:52 +0900

    kakao_3번 문제 해결

--- NBA_heeje/2023_04/week_4th/kakao_3.py
+++ NBA_heeje/2023_04/week_4th/kakao_3.py
@@ -1,2 +1,35 @@
 # [Programmers 2023 KAKAO BLIND TEST] 3. 이모티콘 할인행사
-# 소요 시간 : 00분
+# 소요 시간 : 90분
+
+def solution(users, emoticons):
+    
+    def dfs(idx, discount_rate):
+        nonlocal max_cnt_user, max_benefit
+        if idx == len(emoticons):
+
+            cnt_user = 0
+            total_benefit = 0
+            for i in range(len(users)):
+                price = 0
+                for j in range(len(discount_rate)):
+                    if users[i][0] <= 100 * discount_rate[j]:
+                        price += emoticons[j] * (1 - discount_rate[j])
+                if price >= users[i][1]:
+                    cnt_user += 1
+                else:
+                    total_benefit += price
+            if max_cnt_user < cnt_user:
+                max_cnt_user = cnt_user
+                max_benefit = total_benefit
+            
+            if max_cnt_user == cnt_user and max_benefit < total_benefit:
+                max_benefit = total_benefit
+            return
+        
+        for i in range(4, 0, -1):
+            discount_rate[idx] = 0.1 * i
+            dfs(idx + 1, discount_rate[:])
+    max_cnt_user = 0
+    max_benefit = 0
+    dfs(0, [0] * len(emoticons))
+    return [max_cnt_user, int(max_benefit)]
