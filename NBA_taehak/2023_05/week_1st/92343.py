c2be11b45271da362152a36cddb2ad8dd2e64b0f
happyd918 <0happydeath0@naver.com>
2023-05-06 18:18:11 +0900

    92345번 문제 해결

--- NBA_taehak/2023_05/week_1st/92343.py
+++ NBA_taehak/2023_05/week_1st/92343.py
@@ -1,2 +1,27 @@
 # [Programmers] 92343. 양과 늑대
 # 소요 시간 : 00분
+answer = 0
+def solution(info, edges):
+    visited = [False] * len(info)
+    
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            global answer
+            answer = max(answer, sheep)
+        else:
+            return 
+        
+        for parent, child in edges:
+            # 갈수 있는 곳 == 부모는 방문했고 자식은 방문 안했을때 자식노드로 진행 가능
+            if visited[parent] and not visited[child]:
+                visited[child] = True
+                if info[child] == 0:
+                    dfs(sheep+1, wolf)
+                else:
+                    dfs(sheep, wolf+1)
+                visited[child] = False
+
+    visited[0] = True
+    dfs(1, 0)
+
+    return answer
