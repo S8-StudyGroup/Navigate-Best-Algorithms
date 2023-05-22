ac0ac92083ea10b04ac97e2650b5d09918be0e1b
happyd918 <0happydeath0@naver.com>
2023-05-22 17:02:57 +0900

    20365번 문제 해결

--- NBA_taehak/2023_05/week_3~4/20365.py
+++ NBA_taehak/2023_05/week_3~4/20365.py
@@ -1,2 +1,18 @@
 # [BOJ] 20365. 블로그2
 # 소요 시간 : 00분
+
+color_cnt = input()
+colors = input()
+red = blue = 0
+last = 'Firstcolor'
+
+for color in colors:
+    if color == 'B':
+        if last != color:
+            blue += 1
+    else:
+        if last != color:
+            red += 1
+    last = color
+
+print(min(red, blue) + 1)
