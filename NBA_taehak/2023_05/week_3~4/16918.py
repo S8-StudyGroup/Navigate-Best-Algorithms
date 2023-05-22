89dbadcbd561158216889efdab5ab2345ca2f823
happyd918 <0happydeath0@naver.com>
2023-05-22 17:51:10 +0900

    16918번 문제 해결중

--- NBA_taehak/2023_05/week_3~4/16918.py
+++ NBA_taehak/2023_05/week_3~4/16918.py
@@ -1,2 +1,48 @@
 # [BOJ] 16918. 봄버맨
 # 소요 시간 : 00분
+import sys
+input = sys.stdin.readline
+
+row_size, col_size, times = map(int, input().split())
+case = times % 4
+
+if times == 1:
+    for _ in range(row_size):
+        print(input())
+
+elif case in [0, 2]:
+    for _ in range(row_size):
+        input()
+        print('O'*col_size)
+
+else:
+    area = [list(input()) for _ in range(row_size)]
+
+
+    def cycle():
+        global area
+        bomb = []
+        for r in range(row_size):
+            for c in range(col_size):
+                if area[r][c] == 'O':
+                    bomb.append((r, c))
+        
+        area = [list('O' * col_size) for _ in range(row_size)]
+        delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        for r, c in bomb:
+            area[r][c] = '.'
+
+            for dr, dc in delta:
+                nr = r + dr
+                nc = c + dc
+                if 0 <= nr < row_size and 0 <= nc < col_size:
+                    area[nr][nc] = '.'
+
+
+    cycle()
+    if case == 1:
+        cycle()
+
+    for line in area:
+        print(''.join(line))
