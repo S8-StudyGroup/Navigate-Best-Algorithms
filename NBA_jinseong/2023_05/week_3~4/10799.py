bb4dba19a076a84558d7fcdc02be190aa208e981
ssg-js <likemath1997@gmail.com>
2023-05-22 23:33:33 +0900

    10799번 문제 해결

--- NBA_jinseong/2023_05/week_3~4/10799.py
+++ NBA_jinseong/2023_05/week_3~4/10799.py
@@ -1,2 +1,25 @@
 # [BOJ] 10799. 쇠막대기
-# 소요 시간 : 00분
+# 소요 시간 : 30분
+bar_info = input()
+
+pieces = 0
+stack_cnt = 0
+is_point = False
+
+while len(bar_info) > 0:
+    temp = bar_info[0]
+    bar_info = bar_info[1:]
+    if temp == '(':
+        stack_cnt += 1
+        is_point = True
+    elif temp == ')':
+        stack_cnt -= 1
+        if is_point:
+            pieces += stack_cnt
+            is_point = False
+        else:
+            pieces += 1
+print(pieces)
+
+
+
