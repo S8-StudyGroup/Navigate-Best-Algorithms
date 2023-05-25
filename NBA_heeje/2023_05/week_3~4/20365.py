f8d70ab694600e0dfec7f96c5aabe6fa14763a32
HeeJeYang <ghjadmb21@gmail.com>
2023-05-25 09:54:54 +0900

    20365번 문제 해결

--- NBA_heeje/2023_05/week_3~4/20365.py
+++ NBA_heeje/2023_05/week_3~4/20365.py
@@ -1,2 +1,20 @@
 # [BOJ] 20365. 블로그2
-# 소요 시간 : 00분
+# 소요 시간 : 10분
+
+# 색깔이 변할 때마다 카운트
+# 두 색깔 중 적게 나온것 + 1
+
+N = int(input())
+problems = input()
+colors = [0, 0]
+last = ""
+
+for problem in problems:
+    if problem == "B" and last != "B":
+        colors[0] += 1
+        last = "B"
+    elif problem == "R" and last != "R":
+        colors[1] += 1
+        last = "R"
+
+print(min(colors) + 1)
