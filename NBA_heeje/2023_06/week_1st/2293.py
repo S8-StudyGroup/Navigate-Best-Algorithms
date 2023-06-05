624c7b25fcd625272c6b1991e030c38058da8fea
HeeJeYang <ghjadmb21@gmail.com>
2023-06-05 16:01:30 +0900

    2293번 문제 해결

--- NBA_heeje/2023_06/week_1st/2293.py
+++ NBA_heeje/2023_06/week_1st/2293.py
@@ -1,2 +1,19 @@
 # [BOJ] 2293. 동전 1
-# 소요 시간 : 00분
+# 소요 시간 : 20분
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = [int(input().rstrip()) for _ in range(n)]
+dp = [1] + [0] * k
+
+def sol():
+    for coin in coins:
+        for i in range(coin, k + 1):
+            dp[i] += dp[i - coin]
+        print(dp)
+
+    print(dp[k])
+
+sol()
