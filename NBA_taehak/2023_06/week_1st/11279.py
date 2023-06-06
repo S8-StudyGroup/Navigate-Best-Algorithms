8e2eae7cf60e899f367e711cbe2d8838aea677a7
happyd918 <0happydeath0@naver.com>
2023-06-06 19:30:37 +0900

    11279번 문제해결

--- NBA_taehak/2023_06/week_1st/11279.py
+++ NBA_taehak/2023_06/week_1st/11279.py
@@ -1,2 +1,20 @@
 # [BOJ] 11279. 최대 힙
 # 소요 시간 : 00분
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+
+
+cal_cnt = int(input())
+nums = []
+
+for _ in range(cal_cnt):
+    num = int(input())
+
+    if num == 0:
+        if nums:
+            print(-heappop(nums))
+        else:
+            print(0)
+    else:
+        heappush(nums, -num)
