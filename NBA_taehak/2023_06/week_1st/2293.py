9482df821f7af99a9498b447a444b85c628200a0
happyd918 <0happydeath0@naver.com>
2023-06-06 19:30:24 +0900

    2293번 문제해결

--- NBA_taehak/2023_06/week_1st/2293.py
+++ NBA_taehak/2023_06/week_1st/2293.py
@@ -1,2 +1,17 @@
 # [BOJ] 2293. 동전 1
 # 소요 시간 : 00분
+import sys
+
+input = sys.stdin.readline
+
+coin_cnt, target = map(int, input().split())
+coins = list(int(input()) for _ in range(coin_cnt))
+
+dp = [0] * (target+1)
+dp[0] = 1
+
+for coin in coins:
+    for num in range(coin, target + 1):
+        dp[num] += dp[num-coin]
+
+print(dp[target])
