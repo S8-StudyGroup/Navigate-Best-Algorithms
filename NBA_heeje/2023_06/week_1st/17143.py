d6c816ab86ccd1f821c8600609dc35b6f6e5ab5f
HeeJeYang <ghjadmb21@gmail.com>
2023-06-07 08:32:10 +0900

    17143번 문제 진행 중

--- NBA_heeje/2023_06/week_1st/17143.py
+++ NBA_heeje/2023_06/week_1st/17143.py
@@ -1,2 +1,29 @@
 # [BOJ] 17143. 낚시왕
 # 소요 시간 : 00분
+
+
+def get_shark():
+    shark_size = 0
+    for i in range(R):
+        if matrix[r][c]:
+            total_shark_size += sharks[matrix[r][c][0]][4]
+            del sharks[matrix[r][c][0]]
+            matrix[r][c] = []
+            break
+    
+    return shark_size
+
+
+R, C, M = map(int, input().split())
+matrix = [[[] for _ in range(C)] for _ in range(R)]
+sharks = {}
+total_shark_size = 0
+
+for i in range(M):
+    r, c, s, d, z = map(int, input().split())
+    matrix[r][c].append(i)
+    sharks[i] = [s, d, z]
+
+for king_idx in range(C):
+    get_shark()
+    
