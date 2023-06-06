89e5465da6d28440d7f552b6ae51424d9e433339
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-06-06 15:07:55 +0900

    2293번 문제 해결

--- NBA_seongbin/2023_06/week_1st/2293.py
+++ NBA_seongbin/2023_06/week_1st/2293.py
@@ -1,2 +1,17 @@
 # [BOJ] 2293. 동전 1
-# 소요 시간 : 00분
+# 소요 시간 : 40분
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coin_list = [int(input()) for _ in range(n)]
+dp = [1] + [0] * k
+
+for coin in coin_list:
+    for i in range(coin, k+1):
+        dp[i] += dp[i-coin]
+
+answer = dp[k]
+
+print(answer)
