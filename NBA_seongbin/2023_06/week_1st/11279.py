662ce6bb45a8ce876a76b1725bdead9343b595b4
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-06-06 15:09:37 +0900

    11279번 문제 해결

--- NBA_seongbin/2023_06/week_1st/11279.py
+++ NBA_seongbin/2023_06/week_1st/11279.py
@@ -1,2 +1,19 @@
 # [BOJ] 11279. 최대 힙
 # 소요 시간 : 00분
+import sys
+import heapq
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, -x)
