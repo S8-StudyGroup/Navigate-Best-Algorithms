a0a5fb3a3f4aaa62c81e8755a4a07d2428fa10cd
happyd918 <0happydeath0@naver.com>
2023-06-14 18:58:50 +0900

    1874번 문제해결

--- NBA_taehak/2023_06/week_2nd/1874.py
+++ NBA_taehak/2023_06/week_2nd/1874.py
@@ -1,2 +1,28 @@
 # [BOJ] 1874. 스택 수열
 # 소요 시간 : 00분
+
+n = int(input())
+stack = []
+answer = []
+memo = 0
+now = 1
+
+for i in range(n):
+    num = int(input())
+
+    while now <= num:
+        stack.append(now)
+        answer.append("+")
+        now += 1
+
+    if stack[-1] == num:
+        stack.pop() 
+        answer.append("-")
+    else:   
+        print("NO")
+        memo = 1
+        break
+
+if memo == 0:
+    for i in answer:
+        print(i)
