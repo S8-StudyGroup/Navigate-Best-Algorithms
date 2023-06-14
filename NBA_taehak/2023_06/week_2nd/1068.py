2c59e354bd8cc6d391e7e99d841f817f597920a1
happyd918 <0happydeath0@naver.com>
2023-06-14 18:58:46 +0900

    1068번 문제해결

--- NBA_taehak/2023_06/week_2nd/1068.py
+++ NBA_taehak/2023_06/week_2nd/1068.py
@@ -1,2 +1,24 @@
 # [BOJ] 1068. 트리
 # 소요 시간 : 00분
+
+def dfs(num, arr):
+    arr[num] = -2
+    
+    for i in range(len(arr)):
+        if num == arr[i]:
+            dfs(i, arr)
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+k = int(input())
+count = 0
+
+dfs(k, arr)
+answer = 0
+
+for i in range(len(arr)):
+    if arr[i] != -2 and i not in arr:
+        answer += 1
+
+print(answer)
