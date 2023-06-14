3c6465d40ccaa7521879e6673f08c97c605079a4
happyd918 <0happydeath0@naver.com>
2023-06-14 18:58:58 +0900

    2661번 문제해결

--- NBA_taehak/2023_06/week_2nd/2661.py
+++ NBA_taehak/2023_06/week_2nd/2661.py
@@ -1,2 +1,17 @@
 # [BOJ] 2661. 좋은수열
 # 소요 시간 : 00분
+
+def pick(result, count):
+    for i in range(1, count//2+1):
+        if str(result)[count-i:] == str(result)[count-2*i:count-i]:
+            return
+    if count == N:
+        print(result)
+        exit(0)
+    for i in range(1, 4):
+        temp = result * 10 + i
+        pick(temp, count+1)
+
+
+N = int(input())
+pick(0, 0)
