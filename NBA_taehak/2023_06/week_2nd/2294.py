c522071c544c08a336f675718081b5b24826f50d
happyd918 <0happydeath0@naver.com>
2023-06-14 18:58:54 +0900

    2294번 문제해결

--- NBA_taehak/2023_06/week_2nd/2294.py
+++ NBA_taehak/2023_06/week_2nd/2294.py
@@ -1,2 +1,21 @@
 # [BOJ] 2294. 동전 2
 # 소요 시간 : 00분
+
+n, k = map(int, input().split())
+
+coins =[]
+
+for i in range(n):
+   coins.append(int(input().rstrip()))
+
+dp = [10001] * (k+1)
+dp[0] = 0
+
+for coin in coins:
+   for i in range(coin, k+1):
+       dp[i] = min(dp[i],dp[i-coin]+1)
+       
+if dp[k] == 10001:
+   print(-1)
+else:
+   print(dp[k])
