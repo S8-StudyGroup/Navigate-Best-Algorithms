1b15fe3fd55ea88e77920bbd1e49ec90209bf826
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-06-20 21:50:49 +0900

    1890번 문제 해결

--- NBA_seongbin/2023_06/week_3rd/1890.py
+++ NBA_seongbin/2023_06/week_3rd/1890.py
@@ -1,2 +1,23 @@
 # [BOJ] 1890. 점프
 # 소요 시간 : 00분
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+N = int(input())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * N for _ in range(N)]
+dp[0][0] = 1
+
+for row in range(N):
+    for col in range(N):
+        if row == N - 1 and col == N - 1:
+            break
+        if dp[row][col] > 0:
+            jump = matrix[row][col]
+            if row + jump < N:
+                dp[row + jump][col] += dp[row][col]
+            if col + jump < N:
+                dp[row][col + jump] += dp[row][col]
+
+print(dp[-1][-1])
