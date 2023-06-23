b03d3d0eb39da032d1463194a5c669ec8a8f0887
HeeJeYang <ghjadmb21@gmail.com>
2023-06-24 00:32:57 +0900

    2156번 문제 해결

--- NBA_heeje/2023_06/week_4th/2156.py
+++ NBA_heeje/2023_06/week_4th/2156.py
@@ -1,2 +1,17 @@
 # [BOJ] 2156. 포도주 시식
-# 소요 시간 : 00분
+# 소요 시간 : 20분
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+wines = [int(input()) for _ in range(n)]
+dp = [[0, 0, 0] for _ in range(n)]
+dp[0][1] = wines[0]
+
+for i in range(1, n):
+    dp[i][0] = max(dp[i - 1])
+    dp[i][1] = dp[i - 1][0] + wines[i]
+    dp[i][2] = dp[i - 1][1] + wines[i]
+
+print(max(dp[-1]))
