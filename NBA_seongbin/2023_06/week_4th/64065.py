7e8db6ce0d5ba4508e5c390916290a169d97365e
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-06-27 11:44:17 +0900

    64065번 문제 해결

--- NBA_seongbin/2023_06/week_4th/64065.py
+++ NBA_seongbin/2023_06/week_4th/64065.py
@@ -1,2 +1,25 @@
 # [Programmers] 64065. 튜플
 # 소요 시간 : 00분
+def solution(s):
+    answer = []
+
+    # 문자 s를 리스트로 변경
+    s = list(s[2:-2].split('},{'))
+
+    # 집합의 길이 순으로 정렬
+    sorted_s = sorted(s, key=lambda x: len(x))
+
+    # 요소의 가장 마지막 값을 반환
+    for i in sorted_s:
+        # answer이 비어있지 않을 경우
+        if answer:
+            i = i.split(',')
+            for j in i:
+                if int(j) in answer:
+                    continue
+                else:
+                    answer.append(int(j))
+        else:
+            answer.append(int(i))
+
+    return answer
