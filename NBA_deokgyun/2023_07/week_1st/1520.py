224c4247de2185c8d3705b7a5c1ee56671987688
dokkyunYU <ydgar001@naver.com>
2023-07-11 15:58:39 +0900

    1520번 문제 해결

--- NBA_deokgyun/2023_07/week_1st/1520.py
+++ NBA_deokgyun/2023_07/week_1st/1520.py
@@ -1,2 +1,25 @@
 # [BOJ] 1520. 내리막길
 # 소요 시간 : 00분
+
+row, col = map(int, input().split())
+
+board = [list(map(int, input().split())) for _ in range(row)]
+
+valuelist = []
+
+wayboard = [[0 for _ in range(col)] for _ in range(row)]
+wayboard[0][0] = 1
+
+for i in range(row):
+    for j in range(col):
+        valuelist.append((i,j,board[i][j]))
+
+valuelist.sort(key=lambda x:x[2], reverse=True)
+
+for i,j,value in valuelist:
+    for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+        ni = i + di
+        nj = j + dj
+        if 0 <= ni < row and 0 <= nj < col and board[ni][nj] > board[i][j]:
+            wayboard[i][j] += wayboard[ni][nj]
+print(wayboard[-1][-1])
