9e4e12e1f54ecb8600f705ea4671abf374ef0a9d
HeeJeYang <ghjadmb21@gmail.com>
2023-07-14 22:56:25 +0900

    13335번 문제 해결

--- NBA_heeje/2023_07/week_3rd/13335.py
+++ NBA_heeje/2023_07/week_3rd/13335.py
@@ -1,2 +1,22 @@
 # [BOJ] 13335. 트럭
-# 소요 시간 : 00분
+# 소요 시간 : 15분
+
+n, w, L = map(int, input().split())
+trucks = list(map(int, input().split()))
+bridge = []
+time = 0
+
+while trucks or bridge:
+    time += 1
+    if bridge:
+        for truck in bridge:
+            truck[1] -= 1
+        if bridge[0][1] == 0:
+            bridge.pop(0)                
+
+    if trucks:
+        if trucks[0] + sum(list(map(lambda x: x[0], bridge))) <= L:
+            bridge.append([trucks.pop(0), w])
+
+print(time)
+        
