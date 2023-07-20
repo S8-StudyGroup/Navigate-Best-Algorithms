4f56ae3be975cef75aed817e04564cd5754fa93f
HeeJeYang <ghjadmb21@gmail.com>
2023-07-21 00:52:10 +0900

    15900번 문제 해결

--- NBA_heeje/2023_07/week_4th/15900.py
+++ NBA_heeje/2023_07/week_4th/15900.py
@@ -1,2 +1,40 @@
 # [BOJ] 15900. 나무 탈출
 # 소요 시간 : 00분
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def bfs():
+    queue = deque()
+    queue.append(1)
+    visited = set()
+    visited.add(1)
+    cnt = 0
+    turn = 0
+    while queue:
+        for _ in range(len(queue)):
+            v = queue.popleft()                
+
+            if len(adj_list[v]) == 1:
+                cnt += turn
+
+            for w in adj_list[v]:
+                if w not in visited:
+                    visited.add(w)
+                    queue.append(w)
+
+        turn += 1
+    
+    return "Yes" if cnt % 2 else "No"
+
+
+N = int(input())
+adj_list = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    adj_list[a].append(b)
+    adj_list[b].append(a)
+
+print(bfs())
