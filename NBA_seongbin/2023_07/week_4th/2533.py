cec48b9ec1acde138044ab910ff87f31ce0bb4b6
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-07-26 00:26:37 +0900

    2533번 문제 해결

--- NBA_seongbin/2023_07/week_4th/2533.py
+++ NBA_seongbin/2023_07/week_4th/2533.py
@@ -1,2 +1,36 @@
 # [BOJ] 2533. 사회망 서비스(SNS)
 # 소요 시간 : 00분
+import sys
+sys.setrecursionlimit(10**9)
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+
+def dfs(node):
+    visited[node] = True
+    if len(graph[node]) == 0:
+        dp[node][1] = 1
+        dp[node][0] = 0
+    else:
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                dfs(next_node)
+                dp[node][1] += min(dp[next_node][0], dp[next_node][1])
+                dp[node][0] += dp[next_node][1]
+        dp[node][1] += 1
+
+
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+dp = [[0, 0] for i in range(n + 1)]
+
+for _ in range(n - 1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+visited = [False] * (n + 1)
+
+dfs(1)
+
+print(min(dp[1][0], dp[1][1]))
