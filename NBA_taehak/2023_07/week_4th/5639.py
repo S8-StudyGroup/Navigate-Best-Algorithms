1687a1ed2d6c918bd892b9d35b6809ce862c484f
happyd918 <0happydeath0@naver.com>
2023-07-28 12:42:31 +0900

    5639번 문제해결

--- NBA_taehak/2023_07/week_4th/5639.py
+++ NBA_taehak/2023_07/week_4th/5639.py
@@ -1,2 +1,33 @@
 # [BOJ] 5639. 이진 검색 트리
 # 소요 시간 : 00분
+
+import sys
+sys.setrecursionlimit(10 ** 8)
+input = sys.stdin.readline
+
+# Input
+preorder = []
+while True:
+    try:
+        preorder.append(int(input()))
+    except:
+        break
+
+# postorder
+def postorder(root_idx, end_idx):
+
+    if root_idx > end_idx:
+        return
+    
+    mid = end_idx + 1
+
+    for i in range(root_idx + 1, end_idx + 1):
+        if preorder[i] > preorder[root_idx]:
+            mid = i
+            break
+
+    postorder(root_idx + 1, mid - 1)    # 왼쪽
+    postorder(mid, end_idx)             # 오른쪽
+    print(preorder[root_idx])           # 루트
+
+postorder(0, len(preorder) - 1)
