77c59edd81165946a34b9c2ad564f66e7f39a885
dokkyunYU <ydgar001@naver.com>
2023-08-01 14:36:19 +0900

    1074번 문제 해결

--- NBA_deokgyun/2023_08/week_1st/1074.py
+++ NBA_deokgyun/2023_08/week_1st/1074.py
@@ -1,2 +1,15 @@
 # [BOJ] 1074. Z
 # 소요 시간 : 00분
+
+N, r, c = map(int, input().split())
+
+start_sum = 0
+
+for i in range(N - 1, 0, -1):
+    num = 2**i
+    num2 = num*num
+    start_sum += ((r // num) * 2 + c // num) * num2
+    r = r % num
+    c = c % num
+
+print(start_sum + 2 * r + c)
