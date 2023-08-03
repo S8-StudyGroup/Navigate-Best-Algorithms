58799ecf1631f2172abab9f86de12b9ace82de22
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-08-03 09:30:57 +0900

    12865번 문제 해결

--- NBA_seongbin/2023_08/week_2nd/12865.py
+++ NBA_seongbin/2023_08/week_2nd/12865.py
@@ -1,2 +1,16 @@
 # [BOJ] 12865. 평범한 배낭
 # 소요 시간 : 00분
+n, k = map(int, input().split())
+bags = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        w, v = bags[i - 1]
+
+        if j < w:
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(v + dp[i - 1][j - w], dp[i - 1][j])
+
+print(dp[n][k])
