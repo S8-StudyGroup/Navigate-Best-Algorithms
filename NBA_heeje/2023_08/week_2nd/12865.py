a4517653acdea7c4857176524f700d79c0266f25
HeeJeYang <ghjadmb21@gmail.com>
2023-08-06 12:00:37 +0900

    12865번 문제 해결

--- NBA_heeje/2023_08/week_2nd/12865.py
+++ NBA_heeje/2023_08/week_2nd/12865.py
@@ -1,2 +1,13 @@
 # [BOJ] 12865. 평범한 배낭
-# 소요 시간 : 00분
+# 소요 시간 : 60분
+
+N, K = map(int, input().split())
+things = [tuple(map(int, input().split())) for _ in range(N)]
+dp = [0] * (K + 1)
+
+for i in range(N):
+    for j in range(K + 1):
+        if things[i][0] > j: continue
+        dp[j] = max(things[i][1] + dp[j - things[i][0]], dp[j])
+
+print(dp[-1])
