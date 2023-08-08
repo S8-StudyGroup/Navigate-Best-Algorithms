3838c3e623c3eb13d41a6b573db6066f3aac1c75
happyd918 <0happydeath0@naver.com>
2023-08-08 20:02:04 +0900

    25601번 문제해결

--- NBA_taehak/2023_08/week_2nd/25601.py
+++ NBA_taehak/2023_08/week_2nd/25601.py
@@ -1,2 +1,28 @@
 # [BOJ] 25601. 자바의 형변환
 # 소요 시간 : 00분
+import sys
+input = sys.stdin.readline
+
+# Input
+class_cnt = int(input())
+graph = {}
+for _ in range(class_cnt-1):
+    child, parent = input().split()
+    graph[child] = parent
+A, B = input().split()
+
+# updown
+def updown(child, parent):
+    while child != parent:
+        if graph.get(child) is None:
+            return False
+        child = graph[child]
+    return True
+
+
+# Output
+answer = 0
+if updown(A, B) or updown(B, A):
+    answer = 1
+
+print(answer)
