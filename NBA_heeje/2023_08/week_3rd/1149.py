8f49e897fe0c37790eb09f29f72d28051702388c
HeeJeYang <ghjadmb21@gmail.com>
2023-08-15 21:22:14 +0900

    8월 4주차 문제 업로드

--- NBA_heeje/2023_08/week_3rd/1149.py
+++ NBA_heeje/2023_08/week_3rd/1149.py
@@ -6,12 +6,9 @@ input = sys.stdin.readline
 
 N = int(input())
 houses = [list(map(int, input().split())) for _ in range(N)]
-dp = [[0] * 3 for _ in range(2)]
+dp = [0, 0, 0]
 
 for house in houses:
-    dp[1][0] = house[0] + min(dp[0][1], dp[0][2])
-    dp[1][1] = house[1] + min(dp[0][0], dp[0][2])
-    dp[1][2] = house[2] + min(dp[0][0], dp[0][1])
-    dp[0], dp[1] = dp[1], dp[0]
+    dp[0], dp[1], dp[2] = house[0] + min(dp[1:]), house[1] + min(dp[0], dp[2]), house[2] + min(dp[:2])
 
-print(min(dp[0]))
+print(min(dp))
