b91aa0c8df505e3e62da4d92e255946c1f8ac8a7
HeeJeYang <ghjadmb21@gmail.com>
2023-08-20 10:03:14 +0900

    2343번 문제 해결

--- NBA_heeje/2023_08/week_4th/2343.py
+++ NBA_heeje/2023_08/week_4th/2343.py
@@ -1,2 +1,39 @@
 # [BOJ] 2343. 기타 레슨
-# 소요 시간 : 00분
+# 소요 시간 : 30분
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+lessons = list(map(int, input().split()))
+
+max_length = max(lessons)
+start = 1
+end = 10 ** 9
+answer = end
+
+while start <= end:
+    mid = (start + end) // 2
+
+    if mid < max_length:
+        start = mid + 1
+        continue
+
+    sum = 0
+    cnt = 1
+    for i in range(N):
+        if sum + lessons[i] > mid:
+            cnt += 1
+            sum = lessons[i]
+
+            if cnt > M:
+                start = mid + 1
+                break
+        
+        else:
+            sum += lessons[i]
+    else:
+        end = mid - 1
+        answer = min(answer, mid)
+
+print(answer)
