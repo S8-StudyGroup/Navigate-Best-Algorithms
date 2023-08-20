53eefb3f3d886b1cb7cff6ef92eb2ee5cbe7cbb9
HeeJeYang <ghjadmb21@gmail.com>
2023-08-21 06:50:53 +0900

    17822번 문제 진행 중

--- NBA_heeje/2023_08/week_4th/17822.py
+++ NBA_heeje/2023_08/week_4th/17822.py
@@ -1,2 +1,16 @@
 # [BOJ] 17822. 원판 돌리기
 # 소요 시간 : 00분
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+sum_boards = 0
+
+N, M, T = map(int, input().split())
+boards = [deque(map(int, input().split())) for _ in range(N)]
+
+for _ in range(T):
+    x, d, k = map(int, input().split())
+    mx = x
+    while mx - 1 < len(boards):
+        boards[mx - 1].rotate(k if d == 0 else -k)
