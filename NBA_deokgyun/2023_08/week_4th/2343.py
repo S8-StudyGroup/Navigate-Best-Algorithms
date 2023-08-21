183bd6edfb3e675bcabfaa4d0dae9febcde49ba2
dokkyunYU <ydgar001@naver.com>
2023-08-22 01:33:26 +0900

    2343번 문제 해결

--- NBA_deokgyun/2023_08/week_4th/2343.py
+++ NBA_deokgyun/2023_08/week_4th/2343.py
@@ -1,2 +1,30 @@
 # [BOJ] 2343. 기타 레슨
 # 소요 시간 : 00분
+
+from sys import stdin
+
+readline = lambda : list(map(int, stdin.readline().split()))
+
+num, div = readline()
+lectures = readline()
+
+end = sum(lectures)
+start = max(lectures)
+
+minval = 0
+while end >= start:
+    size = (start + end)//2
+    now = 0
+    cnt = 1
+    for i in range(num):
+        now += lectures[i]
+        if now > size:
+            cnt += 1
+            now = lectures[i]
+            if cnt > div:
+                start = size + 1
+                break
+    else:
+        minval = size
+        end = size - 1
+print(minval)
