6ef6f43a025d0a2a858644beccd37fcbff448a31
happyd918 <0happydeath0@naver.com>
2023-08-22 14:59:36 +0900

    1149번 문제해결

--- NBA_taehak/2023_08/week_3rd/1149.py
+++ NBA_taehak/2023_08/week_3rd/1149.py
@@ -1,2 +1,21 @@
 # [BOJ] 1149. RGB거리
 # 소요 시간 : 00분
+
+# Input
+house_cnt = int(input())
+costs = [tuple(map(int, input().split())) for _ in range(house_cnt)]
+
+# dp
+dp = [[0, 0, 0] for _ in range(house_cnt)]
+dp[0] = list(costs[0])
+
+for idx in range(1, house_cnt):
+    r0, g0, b0 = dp[idx-1]
+    r, g, b = costs[idx]
+
+    dp[idx][0] = min(g0 + r, b0 + r)
+    dp[idx][1] = min(r0 + g, b0 + g)
+    dp[idx][2] = min(r0 + b, g0 + b)
+
+# Output
+print(min(dp[-1]))
