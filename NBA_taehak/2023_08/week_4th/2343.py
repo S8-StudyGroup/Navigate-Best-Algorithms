8613b70552b0c4dce9f1eb894ecba359e2967fb9
happyd918 <0happydeath0@naver.com>
2023-08-29 20:22:09 +0900

    2343번 문제해결

--- NBA_taehak/2023_08/week_4th/2343.py
+++ NBA_taehak/2023_08/week_4th/2343.py
@@ -1,2 +1,29 @@
 # [BOJ] 2343. 기타 레슨
 # 소요 시간 : 00분
+
+# Input
+lec_cnt, blue_cnt = map(int, input().split())
+lectures = list(map(int, input().split()))
+
+# 
+answer = 0
+left, right = max(lectures), sum(lectures)
+while left <= right:
+    mid = (left+right)//2
+
+    count, sum = 0, 0
+    for i in range(lec_cnt):
+        if sum + lectures[i] > mid:
+            count += 1
+            sum = 0
+        sum += lectures[i]
+    if sum:
+        count += 1
+
+    if count > blue_cnt:
+        left = mid + 1
+    else:
+        right = mid - 1
+        answer = mid
+
+print(answer)
