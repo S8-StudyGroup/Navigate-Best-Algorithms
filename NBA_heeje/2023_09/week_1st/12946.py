a89be7563ab381919ea32c7c6d593956b0548cca
HeeJeYang <ghjadmb21@gmail.com>
2023-09-01 07:26:32 +0900

    12946번 문제 해결

--- NBA_heeje/2023_09/week_1st/12946.py
+++ NBA_heeje/2023_09/week_1st/12946.py
@@ -1,2 +1,18 @@
 # [Programmers] 12946. 하노이의 탑
-# 소요 시간 : 00분
+# 소요 시간 : 20분
+
+def solution(n):
+    
+    def move(n, p_from, p_to, p_sub):
+        if n == 1:
+            answer.append([p_from, p_to])
+            return
+        
+        move(n - 1, p_from, p_sub, p_to)
+        answer.append([p_from, p_to])
+        move(n - 1, p_sub, p_to, p_from)
+        
+    answer = []
+    move(n, 1, 3, 2)
+    
+    return answer
