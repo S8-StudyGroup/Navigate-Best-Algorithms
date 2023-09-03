37a6e95a8f69f687ccbfd678ed3fab8505fc12d4
HeeJeYang <ghjadmb21@gmail.com>
2023-09-03 21:22:43 +0900

    17822번 문제 해결

--- NBA_heeje/2023_08/week_4th/17822.py
+++ NBA_heeje/2023_08/week_4th/17822.py
@@ -8,9 +8,17 @@ sum_boards = 0
 
 N, M, T = map(int, input().split())
 boards = [deque(map(int, input().split())) for _ in range(N)]
+cnt_num = N * M
+
+direction = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
 for _ in range(T):
     x, d, k = map(int, input().split())
     mx = x
     while mx - 1 < len(boards):
         boards[mx - 1].rotate(k if d == 0 else -k)
+        mx += x
+
+    if cnt_num == 0: continue
+    for dy, dx in direction:
+        
