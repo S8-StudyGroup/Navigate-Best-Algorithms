03bc7f32f450cea8b8608c57912c1d19d47521c0
HeeJeYang <ghjadmb21@gmail.com>
2023-09-11 00:04:08 +0900

    11052번 문제 해결

--- NBA_heeje/2023_09/week_2nd/11052.py
+++ NBA_heeje/2023_09/week_2nd/11052.py
@@ -1,2 +1,13 @@
 # [BOJ] 11052. 카드 구매하기
 # 소요 시간 : 00분
+
+# 해당 카드팩을 사용하지 않는 경우와
+# 해당 카드팩을 최대치로 사용하는 경우를 비교
+
+N = int(input())
+dp = [0] + list(map(int, input().split()))
+
+for i in range(2, N + 1):
+    dp[i] = max(dp[j] + dp[i - j] for j in range(i // 2 + 1))
+
+print(dp[N])
