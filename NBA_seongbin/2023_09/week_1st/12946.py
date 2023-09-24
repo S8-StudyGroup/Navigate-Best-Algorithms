b03a4db1fc73f447b17fe8c3e056e1e891b6a0c0
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-09-24 18:34:40 +0900

    12946번 문제 해결

--- NBA_seongbin/2023_09/week_1st/12946.py
+++ NBA_seongbin/2023_09/week_1st/12946.py
@@ -1,2 +1,13 @@
 # [Programmers] 12946. 하노이의 탑
 # 소요 시간 : 00분
+def solution(n):
+    def dp(n, start, through, end):
+        if n == 1:
+            return [[start, end]]
+        result = []
+        result.extend(dp(n - 1, start, end, through))
+        result.append([start, end])
+        result.extend(dp(n - 1, through, start, end))
+        return result
+
+    return dp(n, 1, 2, 3)
