c53f15fbc042424ff4e7ab6b57d68ec18f6a5ae5
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-09-24 18:36:22 +0900

    11052번 문제 해결

--- NBA_seongbin/2023_09/week_2nd/11052.py
+++ NBA_seongbin/2023_09/week_2nd/11052.py
@@ -1,2 +1,14 @@
 # [BOJ] 11052. 카드 구매하기
 # 소요 시간 : 00분
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(n+1)]
+
+for i in range(1, n+1):
+  for j in range(1, i+1):
+    dp[i] = max(dp[i], dp[i-j]+li[j])
+
+print(dp[-1])
