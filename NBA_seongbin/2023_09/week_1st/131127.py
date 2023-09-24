9d35e0d14d09338bdaad27b15c446a0034feeeac
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-09-24 18:34:57 +0900

    131127번 문제 해결

--- NBA_seongbin/2023_09/week_1st/131127.py
+++ NBA_seongbin/2023_09/week_1st/131127.py
@@ -1,2 +1,14 @@
 # [Programmers] 131127. 할인 행사
 # 소요 시간 : 00분
+def solution(want, number, discount):
+    answer = 0
+    w = len(want)
+    d = len(discount)
+    want_list = sorted([want[i] for i in range(w) for _ in range(number[i])])
+
+    for i in range(d - 9):
+        cur_want = sorted(discount[i:i+10])
+        if cur_want == want_list:
+            answer += 1
+
+    return answer
