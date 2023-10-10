53035689c9b55ff6ab850695a06be9e77f9fbb9b
happyd918 <0happydeath0@naver.com>
2023-10-10 15:06:08 +0900

    1963번 문제해결

--- NBA_taehak/2023_09/week_2nd/1963.py
+++ NBA_taehak/2023_09/week_2nd/1963.py
@@ -1,2 +1,43 @@
 # [BOJ] 1963. 소수 경로
 # 소요 시간 : 00분
+
+from collections import deque
+
+# Input
+case_cnt = int(input())
+cases = [tuple(input().split()) for _ in range(case_cnt)]
+
+
+# find prime_number
+is_prime = [True] * 10000
+
+for era_num in range(2, 100):
+    if is_prime[era_num]:
+        for n in range(2*era_num, 10000, era_num):
+            is_prime[n] = False
+
+# bfs
+def bfs(start, end):
+    que = deque([(start, 0)])
+    visited = [False] * 10000
+    visited[int(start)] = True
+
+    while que:
+        num, cnt = que.popleft()
+
+        if num == end:
+            return cnt
+        
+        for idx in range(4):
+            for i in range(10):
+                next_num = int(num[:idx] + str(i) + num[idx+1:])
+                if not visited[next_num] and is_prime[next_num] and next_num >= 1000:
+                    visited[next_num] = True
+                    que.append((str(next_num), cnt+1))
+    
+    return 'Impossible'
+
+
+# OutPut
+for start, end in cases:
+    print(bfs(start, end))
