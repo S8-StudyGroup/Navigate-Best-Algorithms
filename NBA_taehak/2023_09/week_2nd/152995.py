3fc9ef5c7b5196a46cc6946eca93158f3f6c030c
happyd918 <0happydeath0@naver.com>
2023-10-10 15:06:36 +0900

    152995번 문제해결

--- NBA_taehak/2023_09/week_2nd/152995.py
+++ NBA_taehak/2023_09/week_2nd/152995.py
@@ -1,2 +1,21 @@
 # [Programmers] 152996. 인사고과
 # 소요 시간 : 00분
+
+def solution(scores):
+    answer = 0
+    target_a, target_b = scores[0]
+    target_score = target_a + target_b
+
+    scores.sort(key=lambda x: (-x[0], x[1]))
+    memo = 0
+    
+    for a, b in scores:
+        if target_a < a and target_b < b:
+            return -1
+        
+        if b >= memo:
+            memo = b
+            if a + b > target_score:
+                answer += 1
+            
+    return answer + 1
